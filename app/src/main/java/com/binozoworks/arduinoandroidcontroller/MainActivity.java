f2b2de98c834aa25fedaf8189ab5c6e7b4615a87
Binozo <noah.czaplewski05@gmail.com>
2021-05-15 13:33:14 +0200

    MainActivity Java Code

--- app/src/main/java/com/binozoworks/arduinoandroidcontroller/MainActivity.java
+++ app/src/main/java/com/binozoworks/arduinoandroidcontroller/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.icu.text.SimpleDateFormat;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -14,7 +15,9 @@ import android.text.format.Formatter;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.io.DataInputStream;
@@ -25,148 +28,120 @@ import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WifiManager wifiManager;
-    WifiConfiguration currentConfig;
-    WifiManager.LocalOnlyHotspotReservation hotspotReservation;
 
+    //General
     public final String TAG = "Arduino Controller";
-    ServerSocket server;
-    final int PORT = 8140;
+
+    //Kommunikation zwischen Arduino und Android
+    NetworkingHelper networkingHelper;
 
     //UI
+    private LinearLayout linearLayout_logs, linearLayout_control;
+    private Button button_verbinden, button_led_an, button_led_aus;
+    private EditText editText_ip;
 
-    private LinearLayout linearLayout_controller, linearLayout_console;
-    private Button button_start;
-    private TextView textView_status, textView_ssid, textView_passwort;
+    //WriteLog
+    SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         init_views();
-
+        register_button_commands();
     }
 
-    void init_views(){
-        linearLayout_controller = findViewById(R.id.linearlayout_arduino_controller);
-        textView_status = findViewById(R.id.textview_status);
-        textView_ssid = findViewById(R.id.textview_ssid);
-        textView_passwort = findViewById(R.id.textview_passwort);
-        linearLayout_console = findViewById(R.id.linearlayout_console);
-        button_start = findViewById(R.id.button_start);
-        button_start.setOnClickListener(new View.OnClickListener() {
+    void register_button_commands(){
+        button_led_aus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                turnOnHotspot();
+                networkingHelper.send_command("led aus");
+            }
+        });
+        button_led_an.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                networkingHelper.send_command("led an");
             }
         });
     }
 
-    public void turnOnHotspot() {
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+    void verbinde_mit_arduino(){
+        if(editText_ip.getText().toString().isEmpty()){
+            editText_ip.setError("Trage bitte eine IP ein");
             return;
         }
 
-        linearLayout_controller.setVisibility(View.VISIBLE);
-        button_start.setVisibility(View.GONE);
+        String arduinoIP = editText_ip.getText().toString();
 
-        wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-
-        wifiManager.startLocalOnlyHotspot(new WifiManager.LocalOnlyHotspotCallback() {
-
+        networkingHelper = new NetworkingHelper(arduinoIP);
+        networkingHelper.connect(new ArduinoEvents() {
             @Override
-            public void onStarted(WifiManager.LocalOnlyHotspotReservation reservation) {
-                super.onStarted(reservation);
-                hotspotReservation = reservation;
-                currentConfig = hotspotReservation.getWifiConfiguration();
-
-                textView_ssid.setText("SSID: " + currentConfig.SSID);
-                textView_passwort.setText("Passwort: " + currentConfig.preSharedKey);
-                textView_status.setText("Status: System läuft");
-
-                Log.v("DANG", "THE PASSWORD IS: "
-                        + currentConfig.preSharedKey
-                        + " \n SSID is : "
-                        + currentConfig.SSID);
-
-                wait_for_arduino();
-
+            public void OnConnectError(String message) {
+                writeLog("Fehler beim Verbinden mit Arduino " + arduinoIP + ": " + message);
             }
 
             @Override
-            public void onStopped() {
-                super.onStopped();
-                Log.v("DANG", "Local Hotspot Stopped");
-                textView_status.setText("Status: Hotspot wurde gestoppt.");
-            }
-
-            @Override
-            public void onFailed(int reason) {
-                super.onFailed(reason);
-                Log.v("DANG", "Local Hotspot failed to start");
-                textView_status.setText("Status: Hotspot konnte nicht starten: " + reason);
-            }
-        }, new Handler());
-    }
-
-    private void wait_for_arduino(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                write_to_console("Warte auf Arduino...");
-                write_to_console("Auf IP " + Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress()) + " und port " + PORT);
-
-                try{
-                    server = new ServerSocket();
-                    server.bind(new InetSocketAddress(PORT)); //sehr wichtig. Wenn fehlt, funktioniert nicht!
-
-                    while(true){
-                        try{
-                            Socket client = server.accept();
-                            DataInputStream input = new DataInputStream(client.getInputStream());
-                            String client_nachricht = input.readUTF();
-                            write_to_console("Nachricht von Client: " + client_nachricht);
-                            DataOutputStream output = new DataOutputStream(client.getOutputStream());
-                            output.writeUTF("Verbindung erfolgreich!");
-                            client.close();
-                        } catch (Exception e){
-                            e.printStackTrace();
-                        }
+            public void OnConnect() {
+                writeLog("Erfolgreich mit Arduino " + arduinoIP + " verbunden! Befehle können jetzt gesendet werden!");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        linearLayout_control.setVisibility(View.VISIBLE);
                     }
-
-
-                }catch (Exception e){
-                    e.printStackTrace();
-                    write_to_console("Fehler: ");
-                    write_to_console(e.getMessage());
-                    write_to_console(e.getCause().toString());
-                }
-
+                });
             }
-        }).start();
+
+            @Override
+            public void OnDisconnect() {
+                writeLog("Von Arduino " + arduinoIP + " getrennt.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        linearLayout_control.setVisibility(View.GONE);
+                    }
+                });
+            }
+
+            @Override
+            public void OnServerMessage(String message) {
+                writeLog("Nachricht von Server: " + message);
+            }
+        });
+
+
+
     }
 
-    private void write_to_console(String text){
+    void writeLog(String message){
+        String time = formatter.format(new Date(System.currentTimeMillis()));
+        TextView textView = new TextView(this);
+        textView.setText("[" + time + "] " + message);
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                TextView textView = new TextView(MainActivity.this);
-                textView.setText(text);
-                linearLayout_console.addView(textView);
+                linearLayout_logs.addView(textView);
             }
         });
+    }
+
+    void init_views(){
+        linearLayout_logs = findViewById(R.id.linearlayout_logs);
+        button_verbinden = findViewById(R.id.button_verbinden);
+        linearLayout_control = findViewById(R.id.linearlayout_control);
+        linearLayout_control.setVisibility(View.GONE);
+        editText_ip = findViewById(R.id.edittext_ip);
+        button_verbinden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verbinde_mit_arduino();
+            }
+        });
+        button_led_an = findViewById(R.id.button_led_an);
+        button_led_aus = findViewById(R.id.button_led_aus);
     }
 }
